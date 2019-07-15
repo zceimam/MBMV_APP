175c1ac7aa1517a2389a2b1df31a76991b6452d2
Valentin Abeille <valentin.abeille@gmail.com>
2019-07-15 16:53:01 +0200

    add booking and flat model

---
+++ db/migrate/20190715143239_create_flats.rb
@@ -0,0 +1,12 @@
+class CreateFlats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flats do |t|
+      t.string :address
+      t.text :description
+      t.integer :price
+      t.references :user, foreign_key:true
+
+      t.timestamps
+    end
+  end
+end
