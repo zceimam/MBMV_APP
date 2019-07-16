df8c99447f84bf5e1cd5aa9331ac269a93a1fca9
Blasco Paterno Castello <beukpaterno@hotmail.fr>
2019-07-16 11:36:27 +0200

    done all the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   resources :flats, except: [:edit, :update, :delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :flats do
+    resources :bookings, only: [:new, :create]
+  end
+
+  get 'profile', to: 'pages#profile', as: :profile
+  get 'bookings', to: 'pages#bookings', as: :bookings
 end
