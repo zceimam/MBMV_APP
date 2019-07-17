d1140cc11064cfd7a88835f63136a5b808d27169
Masha Novikava <mashanovikava@gmail.com>
2019-07-17 15:09:49 +0200

    going to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_17_095037) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +47,6 @@ ActiveRecord::Schema.define(version: 2019_07_17_095037) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.text "description"
     t.boolean "owner", default: false
     t.string "phonenumber"
