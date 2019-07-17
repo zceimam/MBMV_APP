062769fd246ade1d6d72f1e36a0c6233374c72e9
Blasco Paterno Castello <beukpaterno@hotmail.fr>
2019-07-17 12:14:35 +0200

    add a an avatar and a phone number to the user + update the sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_141120) do
+ActiveRecord::Schema.define(version: 2019_07_17_094701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,11 @@ ActiveRecord::Schema.define(version: 2019_07_16_141120) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.text "description"
     t.boolean "owner", default: false
+    t.string "phonenumber"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
