8e15ebb7e5d454fa9639af64d5a25f5887089aa3
Blasco Paterno Castello <beukpaterno@hotmail.fr>
2019-07-17 16:41:23 +0200

    add some style to our website

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_17_095037) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,5 +56,7 @@ ActiveRecord::Schema.define(version: 2019_07_17_095037) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
 end
